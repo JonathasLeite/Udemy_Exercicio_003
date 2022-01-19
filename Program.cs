5a8f45accb06626ca75cb5f3a450455363dbf988
JonathasLeite <jonathas.leite@yahoo.com.br>
2022-01-19 05:30:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_Exercicio_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
